c8f32500b7c91df8bbe4bec050b697e20571af28
Luka Igrutinovic <addictingcode98@outlook.com>
2020-05-18 05:13:46 -0700

    Added option to get correct user's information

--- wTF/wTF/Form1.cs
+++ wTF/wTF/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,8 +13,13 @@ namespace wTF
 {
     public partial class Form1 : Form
     {
+        private UserPanel userPanel;
 
-        string connect = @"server=localhost;userid=root;password=;database=bank";
+
+        public static string usr;
+        public static string pass;
+        public static string connect = @"server=localhost;userid=root;password=;database=bank";
+        public static MySqlConnection con = new MySqlConnection(connect);
         public Form1()
         {
             InitializeComponent();
@@ -21,7 +27,7 @@ namespace wTF
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var con = new MySqlConnection(connect);
+            
             con.Open();
 
             
@@ -35,7 +41,15 @@ namespace wTF
             while (rdr.Read()) {
                 if (rdr.GetString(3) == username.Text && rdr.GetString(4)==password.Text)
                 {
+                    con.Close();
+                    usr = username.Text;
+                    pass = password.Text;
                     MessageBox.Show(this,"Succesfull login","SUCCESS",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                    userPanel = new UserPanel();
+                    userPanel.Show();
+                    rdr.Close();
+                    this.Hide();
+                    break;
                 }
             }
         }
--- wTF/wTF/UserPanel.cs
+++ wTF/wTF/UserPanel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,28 @@ using System.Windows.Forms;
 
 namespace wTF
 {
+
     public partial class UserPanel : Form
     {
+        string connect = Form1.connect;
+        string usr = Form1.usr;
+        string pass= Form1.pass;
+        string expression = "SELECT * FROM users where password="; 
+        MySqlConnection con = Form1.con;
+        
         public UserPanel()
         {
             InitializeComponent();
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand(expression+pass,con);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            if (rdr.Read())
+            {
+                MessageBox.Show(rdr.GetInt32(6).ToString());
+            }
         }
     }
+
+    
 }
